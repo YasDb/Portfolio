d614e2277e15bd5bdd4d71b14b1b146c357c26f7
YasDb <yasmineznt@gmail.com>
2021-03-15 12:27:12 +0100

    firt commit

---
+++ skills.php
@@ -0,0 +1,20 @@
+
+    <section id="skills">
+    <h1 class="skills-title">Skills</h1>
+    <li>
+        <h3>HTML</h3><span class="bar"><span class = "html"></span></span>
+    </li>
+    <li>
+        <h3>CSS</h3><span class="bar"><span class = "css"></span></span>
+    </li>
+    <li>
+        <h3>JAVASCRIPT</h3><span class="bar"><span class = "java"></span></span>
+    </li>
+    <li>
+        <h3>PHP</h3><span class="bar"><span class = "php"></span></span>
+    </li>
+    <li>
+        <h3>SYMPHONY</h3><span class="bar"><span class = "symphony"></span></span>
+    </li>
+    </section>
+    
